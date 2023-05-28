e1e63f39e6af0dc926a87a60638dbcaddd252d11
BEYZA NUR ÖZKAN <172805004@ogr.cbu.edu.tr>
2023-05-28 17:16:11 +0300

    Admin Layout,Vehicle and VehicleType frontend

---
+++ VehicleCardCaseStudy/Controllers/AdminController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VehicleCardCaseStudy.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Vehicle()
+        {
+            return View();
+        }
+        public ActionResult VehicleAdd()
+        {
+            return View();
+        }
+
+        public ActionResult VehicleUpdate()
+        {
+            return View();
+        }
+        public ActionResult VehicleType()
+        {
+            return View();
+        }
+        public ActionResult VehicleTypeAdd()
+        {
+            return View();
+        }
+        public ActionResult VehicleTypeUpdate()
+        {
+            return View();
+        }
+    }
+}
